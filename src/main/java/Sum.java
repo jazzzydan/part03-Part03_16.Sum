621f5f284a823086adafabee71e36401a09fecb4
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 19:00:24 +0200

    Initial commit

---
+++ src/main/java/Sum.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(ArrayList<Integer> numbers) {
+        int sumOfnumbers = 0;
+        for (int iterate: numbers) {
+        sumOfnumbers = sumOfnumbers + iterate;
+        }
+
+        return sumOfnumbers;
+    }
+}
